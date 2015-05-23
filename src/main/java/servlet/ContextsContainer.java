b0fb32693884a629b170aa4d2914b39cfc4b20fe
PaulShestakov <shestakov-pavel@bk.ru>
2015-05-23 13:53:59 +0300

    problems with another user

---
+++ src/main/java/servlet/ContextsContainer.java
@@ -0,0 +1,21 @@
+package servlet;
+
+import javax.servlet.AsyncContext;
+import java.util.ArrayList;
+
+
+/*public class ContextsContainer {
+    ArrayList<AsyncContext> contexts = new ArrayList<>();
+
+
+    public void addContext(AsyncContext context){
+        contexts.add(context);
+    }
+
+    public void executeAllContexts(){
+        for (AsyncContext context: contexts){
+
+        }
+    }
+
+}*/
